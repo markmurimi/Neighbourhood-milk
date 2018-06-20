f61129ff15bb4d69e2ab30744d53edb8f4a522b1
enockomondi <djseanizellkenya@gmail.com>
2018-06-21 00:05:07 +0300

    added africas talking

--- a.py
+++ a.py
@@ -4,5 +4,5 @@ username = "sandbox"    # use 'sandbox' for development in the test environment
 api_key = "c1b52636bea5738374a1566d32b3872bdaeeb0fb83aa50ad562476064cf4413d" # use your sandbox app API key for development in the test environment
 africastalking.initialize("sandbox", "c1b52636bea5738374a1566d32b3872bdaeeb0fb83aa50ad562476064cf4413d")
 sms = africastalking.SMS
-response = sms.send("Jambo Mark! Your order is being processed and will be delivered in less than 30 minutes! thank you for being part of us", ["+254726830125"])
+response = sms.send("an order has been made,check your order list", ["+254726830125"],["+254726830125"])
 print(response)
